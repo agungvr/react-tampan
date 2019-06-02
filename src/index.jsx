63ff4d0a9ac6108f68c6cdd58540c02690b66403
Agung Nugraha <agungvr@gmail.com>
2019-06-02 11:53:54 +0700

    update

--- src/index.jsx
+++ src/index.jsx
@@ -13,18 +13,4 @@ Sentry.init({
 const App = AppKernel(Root);
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-serviceWorker.register({
-  onUpdate: registration => {
-    const waitingServiceWorker = registration.waiting;
-
-    if (waitingServiceWorker) {
-      waitingServiceWorker.addEventListener("statechange", event => {
-        if (event.target.state === "activated") {
-          window.location.reload();
-        }
-      });
-      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
-    }
-  }
-});
+serviceWorker.register();
